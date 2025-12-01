4014aa0de2db30cd894b571f5007f62f8c0b3533
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2025-12-01 09:36:33 -0300

    .

---
+++ app/lua/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { LuaView } from "@/components/html-views/LuaView";
+
+export default function LuaPage() {
+  return (
+    <main className="min-h-screen bg-black">
+      <LuaView />
+    </main>
+  );
+}
