c7691599dbc0b96b20709d0871ea16890fd0d7b2
talitadeoa <edtaraujo@gmail.com>
2026-01-17 19:46:13 -0300

    feat: add SavedTodosPanel component and update index exports

--- app/cosmos/planeta/components/index.ts
+++ app/cosmos/planeta/components/index.ts
@@ -2,5 +2,5 @@ export { FiltersPanel } from './FiltersPanel';
 export { IslandsList } from './IslandsList';
 export { MoonCluster } from './MoonCluster';
 export { default as PlanetScreen } from './PlanetScreen';
-export { SavedTodosPanel } from '@/app/cosmos/planeta/components/SavedTodosPanel';
+export { SavedTodosPanel } from './SavedTodosPanel';
 export { default as TodoInput } from '@/app/cosmos/components/TodoInput';
